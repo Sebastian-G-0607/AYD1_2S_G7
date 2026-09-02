a05fcf1abdbd54af9f00d7cfde83f7e158cfc993
Christian Chinchilla <christianchinchilla36@gmail.com>
2026-09-01 19:44:22 -0600

    feat(horarios): agregar interfaz y lógica de configuracion de horarios

---
+++ edu-connect-client/src/pages/TutorSchedulePage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DashboardLayout from '@/layouts/DashboardLayout.vue'
+import { TutorScheduleView } from '@/features/tutor-schedule'
+</script>
+
+<template>
+  <DashboardLayout>
+    <TutorScheduleView />
+  </DashboardLayout>
+</template>
